407ef4d14e04fb36ceeeec22d3089ee84139dc7e
Nopri010 <108908521+Nopri010@users.noreply.github.com>
2024-05-29 20:50:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ tela = pygame.display.set_mode(tamanho)
 pygame.display.set_caption("Iron Man")
 branco = (255, 255, 255)
 preto = (0, 0, 0)
+iron = pygame.image.load("assets/iron.png")
+fundo = pygame.image.load("assets/fundo.png")
 posicaoXPersona = 400
 posicaoYPersona = 300
 movimentoXPersona = 0
+movimentoYPersona = 0
+fonte = pygame.font.SysFont("comicsans", 14)
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -18,9 +22,9 @@ while True:
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_LEFT:
             movimentoXPersona =  - 5 
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP:
-            movimentoYPersona =  + 5
+            movimentoYPersona =  - 5
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_DOWN:
-            movimentoYPersona =  - 5 
+            movimentoYPersona =  + 5 
         elif evento.type == pygame.KEYUP and evento.key == pygame.K_RIGHT:
             movimentoXPersona =  0
         elif evento.type == pygame.KEYUP and evento.key == pygame.K_LEFT:
@@ -29,7 +33,21 @@ while True:
             movimentoYPersona =  0
         elif evento.type == pygame.KEYUP and evento.key == pygame.K_DOWN:
             movimentoYPersona =  0
+    posicaoXPersona = posicaoXPersona + movimentoXPersona
+    posicaoYPersona = posicaoYPersona + movimentoYPersona
+    if posicaoXPersona <  0:
+        posicaoXPersona =  10
+    elif posicaoXPersona > 550:
+        posicaoXPersona = 540
+    if posicaoYPersona < 0 :
+        posicaoYPersona = 10
+    elif posicaoYPersona > 473:
+        posicaoYPersona = 463
     tela.fill(branco)
-    pygame.draw.circle(tela, preto, (400, 300), 40, 0)
+    tela.blit(fundo, (0,0))
+    #pygame.draw.circle(tela, preto, (posicaoXPersona, posicaoYPersona), 40, 0)
+    tela.blit(iron, (posicaoXPersona, posicaoYPersona))
+    #texto = fonte.render(str(posicaoXPersona)+"-"+str(posicaoYPersona), True, branco)
+    #tela.blit(texto, (posicaoXPersona-30, posicaoYPersona-10))
     pygame.display.update()
     relogio.tick(60)
