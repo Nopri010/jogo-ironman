b1a41e4a6cae451feef70d7c4491475a07cfd0e9
Nopri010 <108908521+Nopri010@users.noreply.github.com>
2024-06-03 19:42:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ tamanho = (800,600)
 relogio = pygame.time.Clock()
 tela = pygame.display.set_mode(tamanho)
 pygame.display.set_caption("Iron Man")
-branco = (255, 255, 255)
-preto = (0, 0, 0)
+
+
 iron = pygame.image.load("assets/iron.png")
 fundo = pygame.image.load("assets/fundo.png")
 missel = pygame.image.load("assets/missile.png")
+
+
+branco = (255, 255, 255)
+preto = (0, 0, 0)
 posicaoXPersona = 400
 posicaoYPersona = 300
 movimentoXPersona = 0
@@ -17,12 +21,18 @@ movimentoYPersona = 0
 posicaoXMissel = 400
 posicaoYMissel = -240
 velcidadeM = 1
+pontos = 0
+
+
 fonte = pygame.font.SysFont("comicsans", 14)
 pygame.mixer.music.load("assets/ironsound.mp3")
 pygame.mixer.music.play(-1)
 missileSound = pygame.mixer.Sound("assets/missile.wav")
 pygame.mixer.Sound.play(missileSound)
+
 while True:
+    
+    
     
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -43,8 +53,15 @@ while True:
             movimentoYPersona =  0
         elif evento.type == pygame.KEYUP and evento.key == pygame.K_DOWN:
             movimentoYPersona =  0
+            
+            
+            
     posicaoXPersona = posicaoXPersona + movimentoXPersona
     posicaoYPersona = posicaoYPersona + movimentoYPersona
+    
+    
+    
+    
     if posicaoXPersona <  0:
         posicaoXPersona =  10
     elif posicaoXPersona > 550:
@@ -53,18 +70,28 @@ while True:
         posicaoYPersona = 10
     elif posicaoYPersona > 473:
         posicaoYPersona = 463
+        
+        
+        
+        
     tela.fill(branco)
     tela.blit(fundo, (0,0))
     #pygame.draw.circle(tela, preto, (posicaoXPersona, posicaoYPersona), 40, 0)
     tela.blit(iron, (posicaoXPersona, posicaoYPersona))
+    
+    
     posicaoYMissel = posicaoYMissel + velcidadeM
     if posicaoYMissel > 600:
         posicaoYMissel = -240
+        pontos = pontos + 1
         velcidadeM = velcidadeM + 1
         posicaoXMissel = random.randint(0,800)
         pygame.mixer.Sound.play(missileSound)
+        
+        
+        
     tela.blit( missel, (posicaoXMissel, posicaoYMissel))
-    #texto = fonte.render(str(posicaoXPersona)+"-"+str(posicaoYPersona), True, branco)
-    #tela.blit(texto, (posicaoXPersona-30, posicaoYPersona-10))
+    texto = fonte.render("Pontos: "+str(pontos), True, branco)
+    tela.blit(texto, (10,10))
     pygame.display.update()
     relogio.tick(60)
